6bdb1f7bbea3b90cea545160e1a553d5fb70046f
Alexander Charette <ga_charette@pm.me>
2022-12-06 10:38:03 +0100

    Add command to list keys for visibility

--- run-backup.sh
+++ run-backup.sh
@@ -4,6 +4,11 @@ filename=$CLINIC_SLUG.$(date +%Y%m%d-%H%M%S)
 fullFilename="hdc-${filename}.sql"
 folder=$(date +%Y%m)
 
+echo "Importing gpg key"
+gpg --import hdcprod.pgp 
+
+gpg --list-keys
+
 kubectl exec -n $CLINIC_SLUG db-0 -c mariadb -- bash -c "mysqldump -uroot -p${MYSQL_ROOT_PASSWORD} oscar \
     allergies \
     appointment \
@@ -33,9 +38,6 @@ kubectl exec -n $CLINIC_SLUG db-0 -c mariadb -- bash -c "mysqldump -uroot -p${MY
     provider > ${fullFilename}"
 
 kubectl cp -n $CLINIC_SLUG db-0:/code/$fullFilename /$fullFilename
-
-echo "Importing gpg key"
-gpg --import hdcprod.pgp
 
 # encrypt
 # always trust the key, it's verified manually.
